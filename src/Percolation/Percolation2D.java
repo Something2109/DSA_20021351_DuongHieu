7a8d0b1d6f169099d3577b885eee50d56a5e3878
Something2109 <hieuduong21092002@outlook.com>
2021-09-24 18:28:31 +0700

    Improve Percolation
    Rearrange code

--- src/Percolation/Percolation2D.java
+++ src/Percolation/Percolation2D.java
@@ -2,35 +2,46 @@ package Percolation;
 import edu.princeton.cs.algs4.*;
 
 public class Percolation2D {
-    int field, edge;
+    int field, side;
     UF uf;
     boolean[] tileStatus;
+    int tileCount;
 
     Percolation2D(int n) {
-        edge = n;
+        side = n;
         field = n * n + 2;
         uf = new UF(field);
         tileStatus = new boolean[field];
-        StdOut.println("Create field: " + edge + ", " + field);
+        tileCount = 0;
+        StdOut.println("Create field: " + side + ", " + field);
     }
 
     public static void main(String[] args) {
         int n = StdIn.readInt();
-        int tileCount = 0;
-        Percolation2D per = new Percolation2D(n);
-        per.settingUp();
-        while (!per.uf.connected((per.field - 2), (per.field - 1))) {
-            per.addTile();
+        double count = 0;
+        for (int i = 1; i <= 100; i++) {
+            StdOut.println("Attemp " + i +"\n");
+            Percolation2D per = new Percolation2D(n);
+            count += per.run();
+            StdOut.println("\n");
+        }
+        StdOut.println(count / (n * n * 100));
+    }
+
+    public int run() {
+        settingUp();
+        while (!uf.connected((field - 2), (field - 1))) {
+            addTile();
             tileCount ++;
         }
-        StdOut.println(tileCount);
+        return tileCount;
     }
 
     private void settingUp() {
-        for (int i = 0; i < edge; i++) {
-            Tile2D tile = new Tile2D(0, i, edge);
+        for (int i = 0; i < side; i++) {
+            Tile2D tile = new Tile2D(0, i, side);
             uf.union(field - 2, tile.getTileID());
-            Tile2D tile1 = new Tile2D(edge - 2, i , edge);
+            Tile2D tile1 = new Tile2D(side - 2, i , side);
             uf.union(field - 1, tile1.getTileID());
         }
         StdOut.println();
@@ -39,7 +50,7 @@ public class Percolation2D {
     private void addTile() {
         Tile2D tile;
         do  {
-            tile = new Tile2D(edge);
+            tile = new Tile2D(side);
         } 
         while (tileStatus[tile.getTileID()]);
         tileStatus[tile.getTileID()] = true;
@@ -49,18 +60,18 @@ public class Percolation2D {
     }
 
     private void connectTile(Tile2D tile) {
-        Tile2D nextTile = new Tile2D(tile, -1, 0, edge);
+        Tile2D nextTile = new Tile2D(tile, -1, 0, side);
         unionTile(tile, nextTile);
-        nextTile.setFromTile(tile, 1, 0, edge);
+        nextTile.setFromTile(tile, 1, 0, side);
         unionTile(tile, nextTile);
-        nextTile.setFromTile(tile, 0, -1, edge);
+        nextTile.setFromTile(tile, 0, -1, side);
         unionTile(tile, nextTile);
-        nextTile.setFromTile(tile, 0, 1, edge);
+        nextTile.setFromTile(tile, 0, 1, side);
         unionTile(tile, nextTile);
     }
 
     private void unionTile(Tile2D tile, Tile2D nextTile) {
-        if (nextTile.valid(edge) && tileStatus[nextTile.getTileID()]) {
+        if (nextTile.valid(side) && tileStatus[nextTile.getTileID()]) {
             uf.union(tile.getTileID(), nextTile.getTileID());
             StdOut.println("Connect tile: " + tile.toString() + " " + nextTile.toString());
         }
--- src/Percolation/Percolation3D.java
+++ src/Percolation/Percolation3D.java
@@ -2,36 +2,47 @@ package Percolation;
 import edu.princeton.cs.algs4.*;
 
 public class Percolation3D {
-    int field, edge;
+    int field, side;
     UF uf;
     boolean[] tileStatus;
+    int tileCount;
 
     Percolation3D(int n) {
-        edge = n;
+        side = n;
         field = n * n * n + 2;
         uf = new UF(field);
         tileStatus = new boolean[field];
-        StdOut.println("Create field: " + edge + ", " + field);
+        tileCount = 0;
+        StdOut.println("Create field: " + side + ", " + field);
     }
 
     public static void main(String[] args) {
         int n = StdIn.readInt();
-        int tileCount = 0;
-        Percolation3D per = new Percolation3D(n);
-        per.settingUp();
-        while (!per.uf.connected((per.field - 2), (per.field - 1))) {
-            per.addTile();
+        double count = 0;
+        for (int i = 1; i <= 100; i++) {
+            StdOut.println("Attemp " + i +"\n");
+            Percolation3D per = new Percolation3D(n);
+            count += per.run();;
+            StdOut.println("\n");
+        }
+        StdOut.println(count / (n * n * n * 100));
+    }
+
+    public int run() {
+        settingUp();
+        while (!uf.connected((field - 2), (field - 1))) {
+            addTile();
             tileCount ++;
         }
-        StdOut.println(tileCount);
+        return tileCount;
     }
 
     private void settingUp() {
-        for (int i = 0; i < edge; i++) {
-            for (int j = 0; j < edge; j++) {
-                Tile3D tile = new Tile3D(i, j, 0, edge);
+        for (int i = 0; i < side; i++) {
+            for (int j = 0; j < side; j++) {
+                Tile3D tile = new Tile3D(i, j, 0, side);
                 uf.union(field - 2, tile.getTileID());
-                Tile3D tile1 = new Tile3D(i, j, edge - 1, edge);
+                Tile3D tile1 = new Tile3D(i, j, side - 1, side);
                 uf.union(field - 1, tile1.getTileID());
             }
         }
@@ -41,7 +52,7 @@ public class Percolation3D {
     private void addTile() {
         Tile3D tile;
         do  {
-            tile = new Tile3D(edge);
+            tile = new Tile3D(side);
         } 
         while (tileStatus[tile.getTileID()]);
         tileStatus[tile.getTileID()] = true;
@@ -51,22 +62,22 @@ public class Percolation3D {
     }
 
     private void connectTile(Tile3D tile) {
-        Tile3D nextTile = new Tile3D(tile, -1, 0, 0,edge);
+        Tile3D nextTile = new Tile3D(tile, -1, 0, 0,side);
         unionTile(tile, nextTile);
-        nextTile.setFromTile(tile, 1, 0, 0, edge);
+        nextTile.setFromTile(tile, 1, 0, 0, side);
         unionTile(tile, nextTile);
-        nextTile.setFromTile(tile, 0, -1, 0, edge);
+        nextTile.setFromTile(tile, 0, -1, 0, side);
         unionTile(tile, nextTile);
-        nextTile.setFromTile(tile, 0, 1, 0, edge);
+        nextTile.setFromTile(tile, 0, 1, 0, side);
         unionTile(tile, nextTile);
-        nextTile.setFromTile(tile, 0, 0, -1, edge);
+        nextTile.setFromTile(tile, 0, 0, -1, side);
         unionTile(tile, nextTile);
-        nextTile.setFromTile(tile, 0, 0, 1, edge);
+        nextTile.setFromTile(tile, 0, 0, 1, side);
         unionTile(tile, nextTile);
     }
 
     private void unionTile(Tile3D tile, Tile3D nextTile) {
-        if (nextTile.valid(edge) && tileStatus[nextTile.getTileID()]) {
+        if (nextTile.valid(side) && tileStatus[nextTile.getTileID()]) {
             uf.union(tile.getTileID(), nextTile.getTileID());
             StdOut.println("Connect tile: " + tile.toString() + " " + nextTile.toString());
         }
